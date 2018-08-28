6991b3fa0d37f184a2130b7d5764110336ea389d
Renée Kooi <renee@kooi.me>
2018-08-28 18:56:57 +0200

    Add scenario filename

--- views/scxView.js
+++ views/scxView.js
@@ -11,8 +11,9 @@ module.exports = function scxView (state, emit) {
 
   return html`
     <div>
-      <div class="flex justify-start items-start">
+      <div class="flex justify-between items-start">
         <div class="bg-white pa3 ma2 shadow-2">
+          <h2 class="code ma0 mb2 tc">${scx.header.filename}</h2>
           ${minimap.render(scx.map)}
         </div>
       </div>
